344e4cdca1fcc5bb079c661f8cde1ada3f2ed3a0
aleesha-wood <spectrat@spectratart.com>
2019-10-14 18:04:26 -0400

    reformatted formik form, added validations

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserForm from "./components/Form.js"
+import FormikUserForm from "./components/Form.js"
 
 function App() {
   return (
     <div className="App">
       <h1>Sign Up</h1>
-      <UserForm />
+      <FormikUserForm />
     </div>
   );
 }
--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,64 +1,84 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
+import { withFormik, Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import '../Form.css';
 
-const UserForm = props => {
-
+function UserForm() {
   return (
-    <Formik
-      initialValues={{
-        name: '', 
-        email: '', 
-        password: '',
-        TOS: false 
-      }}
-      onSubmit={(values, tools) => {
-        console.log("values: ",values,"tools: ", tools);
-        tools.resetForm();
-      }}
+    <Form className='user-form'>
+      <Field 
+        className = 'text-field'
+        type = 'text'
+        name = 'name'
+        placeholder = 'name'
+      />
+      <Field 
+        className = 'text-field'
+        type = 'text'
+        name = 'email'
+        placeholder = 'email' 
+      />
+      <Field 
+        className = 'text-field'
+        type = 'password'
+        name = 'password'
+        placeholder = 'Password' 
+      />
+      <div className='tos-submit'>
+        <label>
+        <Field
+          type = 'checkbox'
+          name = 'TOS'
+        />
+        I Agree to the Terms of Service
+        </label>
+        <input 
+          className = 'sub-btn'
+          type='submit' 
+          value='Sign Up!'
+        />
+      </div>
+    </Form>
+  );
+}
 
-      render={props => {
-        return (
-          <Form className='user-form'>
-            <Field 
-              className = 'text-field'
-              type = 'text'
-              name = 'name'
-              placeholder = 'name'
-            />
-            <Field 
-              className = 'text-field'
-              type = 'text'
-              name = 'email'
-              placeholder = 'email' 
-            />
-            <Field 
-              className = 'text-field'
-              type = 'password'
-              name = 'password'
-              placeholder = 'Password' 
-            />
-            <div className='tos-submit'>
-              <label>
-              <Field
-                type = 'checkbox'
-                name = 'TOS'
-              />
-              I Agree to the Terms of Service
-              </label>
-              <input 
-                className = 'sub-btn'
-                type='submit' 
-                value='Sign Up!'
-              />
-            </div>
-          </Form>
-        )
-      }}
-    />
-  )
-};
+const FormikUserForm = withFormik({
+  mapPropsToValues ({name, email, password, TOS}) {
+    return {
+      name: name ||"",
+      email: email || "",
+      password: password || "",
+      TOS: true || false
+    };
+  },
+  //VALIDATION
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .required("Please type your name."),
+    email: Yup.string()
+      .email()
+      .required("Please type a valid email."),
+    password: Yup.string()
+      .min(8)
+      .required("Please choose a password with at least 8 characters"),
+    TOS: Yup.bool()
+      .positive()
+      .required("Terms of Service Agreement required!")
+  }),
+  //END VALIDATION
+  
+  handleSubmit(values) {
+    console.log(values)
+    //stuff goes here too
+  }
+})(UserForm);
 
-export default UserForm;
+export default FormikUserForm;
+
+// onSubmit={(values, tools) => {
+//   console.log("values: ",values,"tools: ", tools);
+//   tools.resetForm();
+// }}
+
+//place this later...alter for stuff.
